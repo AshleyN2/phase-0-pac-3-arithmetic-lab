b7f7e971d7eda80f3b9f16f6e62e786365bdbe13
Ashley <ashleynjeri7@gmail.com>
2022-08-13 18:54:12 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+function add() {
+    
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+function increment(n){
+    return ++n;
+}
+
+function decrement(n){
+    return --n;
+}
+
+
+function makeInt(n){
+    parseInt('n', 10);
+    return n;
+}
+
+function preserveDecimal(n){
+    parseFloat(n);
+    return n;
+}
